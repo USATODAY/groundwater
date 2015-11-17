e8e446c13eb6c46d0d7f9e802f986907beeb068a
mitchthorson <mthorson@usatoday.com>
2015-11-17 16:28:58 -0500

    add require config file to use for shared libraries

---
+++ graphics/require.config.js
@@ -0,0 +1,8 @@
+/*
+ *Store require paths for shared libraries that will run on pages in this file. This must be loaded onto each page that runs these graphics.
+ */
+require.config({
+    paths: {
+        'd3': 'http://www.gannett-cdn.com/experiments/usatoday/_common/_scripts/d3.min.js'
+    }
+})
