12c71e480b1df3ba8e683e7c202894a944aa01ff
Jon Dang <jondang@gmail.com>
2015-11-20 14:19:01 -0500

    new shared-assets dir with reference to script tags and common gig-embed class css

--- graphics/require.config.js
+++
@@ -1,9 +0,0 @@
-/*
- *Store require paths for shared libraries that will run on pages in this file. This must be loaded onto each page that runs these graphics.
- */
-require.config({
-    paths: {
-        'd3': 'http://www.gannett-cdn.com/experiments/usatoday/_common/_scripts/d3.min.js',
-        'topojson': 'http://www.gannett-cdn.com/experiments/usatoday/_common/_scripts/topojson.v1.min.js'
-    }
-})
